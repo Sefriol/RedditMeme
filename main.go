bd567353b4ed93fe5462030a4e48529fb2716c9e
Sefriol <georgepatton1945@hotmail.com>
2016-01-21 00:36:01 +0200

    Test Loop.

--- main.go
+++ main.go
@@ -5,11 +5,16 @@ import (
     "fmt"
     "os"
     "strings"
-    "net/url"
 	"github.com/jzelinskie/geddit"
+    "sort"
 )
-
+//Match ...
+type Match struct {
+    CommentID string
+    Answered bool
+}
 var session *geddit.LoginSession
+var matches []*Match
 // Please don't handle errors this way.
 func main() {
     //Read reddit username and password
@@ -49,25 +54,27 @@ func main() {
         if err != nil {
             continue
         }
-        comments = append(comments, commentDetect("Except that's what they do. Riki has had so many reworks with the intention of making the hero competitively viable instead of just buffing him because it WOULD break 1-2k. They are actively trying to find a position for him everywhere, not decent in competitive and literally unbeatable for lower skill.", comment)...)
+        comments = append(
+            comments, 
+            commentDetect("It should hit all non creep units IMO, much like you don't pick PL into Ember you wouldn't pick PL into Riki. Ember in a way has a 5 second CD Riki ult", comment)...)
     }
     fmt.Printf("Len commets: %d\n\n", len(comments))
-    fmt.Print("Enter Meme: ")
-    urlquery, _ := reader.ReadString('\n')
-    urlquery = url.QueryEscape(strings.TrimSpace(urlquery))
-    fmt.Printf("5/7 %s\n\n",urlquery)
     
-    knowyourmemes, _ := getMemes(urlquery)
-    for _, s := range knowyourmemes {
-        fmt.Printf("Meme: %d %s\n\n", len(knowyourmemes), s.Body)
-    }
+    // fmt.Print("Enter Meme: ")
+    // urlquery, _ := reader.ReadString('\n')
+    // urlquery = url.QueryEscape(strings.TrimSpace(urlquery))
+    // fmt.Printf("5/7 %s\n\n",urlquery)
+    
+    // knowyourmemes, _ := getMemes(urlquery)
+    // for _, s := range knowyourmemes {
+    //     fmt.Printf("Meme: %d %s\n\n", len(knowyourmemes), s.Body)
+    // }
     //http://rkgk.api.searchify.com/v1/indexes/kym_production/instantlinks?query=jotain&fetch=*
     
     // Print title and author of each submission
-    // comments, _ := session.Comments(submissions[0])
-    // for _, c := range comments {
-    //      fmt.Printf("Comment: %s\n\n",c.String())
-    // }
+    for _, c := range comments {
+         fmt.Printf("id:%s Comment: %s\n\n",c.FullID, c.String())
+    }
     // for _, s := range submissions {
         // 	fmt.Printf("Title: %s\nAuthor: %s Comments: %s\n\n", s.Title, s.Author,comments)
     // }
@@ -78,9 +85,24 @@ func main() {
 
 func commentDetect(detect string, comments []*geddit.Comment)([]*geddit.Comment)  {
     var finalcomments []*geddit.Comment
+    var temp *Match
+    
     for _, c := range comments {
         if strings.Contains(c.Body, detect) {
-            finalcomments = append(finalcomments, c)
+            i := sort.Search(len(matches),func(i int) bool { return matches[i].CommentID >= c.FullID })
+            if i < len(matches) && matches[i].CommentID == c.FullID {
+                fmt.Printf("Match found. Do nothing.\n\n")
+                // Match found. Do nothing.
+            } else {
+                finalcomments = append(finalcomments, c)
+                matches = append(matches, temp)
+                copy(matches[i+1:], matches[i:])
+                new := Match{
+                    Answered: false,
+                    CommentID: c.FullID,
+                }
+                matches[i] = &new
+            }
         }
         if len(c.Replies) > 0 {
             finalcomments = append(finalcomments,commentDetect(detect, c.Replies)...)
